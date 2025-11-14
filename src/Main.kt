116e5fd5a98aae6062b6b9b709eb539491a4b0fa
mishapentii <mihai.pentii.7e9@itb.cat>
2025-11-14 08:12:36 +0100

    Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -11,7 +11,6 @@ fun main(){
     println("Benvingut/da al joc dels daus.\nPer guanyar cada partida, la suma dels punts de les teves tirades dels teus daus ha de ser superior a la de la CPU")
     println(DAUS)
 
-    // Llegim el número de partides que volem jugar
     do {
         println("Quantes partides vols jugar? (de 1 a 3)")
         partides = readLine()?.toIntOrNull()
@@ -22,7 +21,6 @@ fun main(){
         }
     }while(partides == null)
 
-    // Llegim el número de quantes tirades volem fer per cada partida
     do {
         println("Quantes tirades vols fer per cada partida? (de 1 a 6)")
         tiradesPerPartida = readLine()?.toIntOrNull()
@@ -33,30 +31,23 @@ fun main(){
         }
     }while(tiradesPerPartida == null)
 
-    // Declarem la matriu
     var tiradesGuardades: Array<IntArray>
 
-    // Inicialitzem la matriu de partides files i (tiradesPerPartida + 1) columnes
     tiradesGuardades = Array(partides){IntArray((tiradesPerPartida + 1)) }
 
-    // Repetim tantes vegades com partides
     for(partida in 0 until partides) {
         var acumuladorCPU: Int = 0
         var tiradaActual: Int = 0
 
         for (tirada in 0 until tiradesGuardades[partida].size - 1) {
-            /** Tirades persona **/
             println("Tira el dau! (Intent $tirada)")
             tiradaActual = Random.nextInt(1, 6 + 1)
             println("Has tret un ${CARES_DAU[tiradaActual-1]} !")
 
-            // Guardem la tirada
             tiradesGuardades[partida][tirada] = tiradaActual
 
-            // Acumulem el sumatori a l'última columna de la fila
             tiradesGuardades[partida][tiradesPerPartida] += tiradaActual
 
-            /** Tirades CPU **/
             acumuladorCPU += Random.nextInt(1, 6 + 1)
         }
 
